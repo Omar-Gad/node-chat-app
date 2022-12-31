104713462ab8607d78da32cd84143cee81e9ebfc
Omar-Gad <omargad9000@gmail.com>
2022-12-31 11:45:16 +0200

    rsa trial 1

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 var cors = require('cors')
 const http = require('http');
 const { Server } = require("socket.io");
+const RSA = require('./utils/rsa')
 
 const app = express();
 app.use(cors({ origin: 'http://localhost:3000' }))
@@ -9,7 +10,7 @@ app.use(cors({ origin: 'http://localhost:3000' }))
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: '*', }
+  cors: { origin: 'http://localhost:3000', }
 });
 
 
@@ -28,3 +29,28 @@ io.on('connection', (socket) => {
   })
 });
 
+(() => {
+  const msg = 'hello world'
+  const p = RSA.randomPrime(32)
+  const q = RSA.randomPrime(32)
+
+  const encoded = Buffer.from(msg).toString('hex');
+  const decoded = Buffer.from(encoded, 'hex').toString();
+
+  const key = RSA.generate(64, p, q)
+  // const encodedMsg = RSA.encode(msg)
+  const C = RSA.encrypt(9, key.e, key.n)
+  const M = RSA.decrypt(C, key.d, key.n)
+  // const decodedMsg = RSA.decode(M)
+
+  console.log('msg', decodedMsg)
+
+})();
+
+/* 
+1 - create key pairs in client using RSA
+2 - send public key to the server
+3 - server encrypts 3 DES-keys using public key
+4 - client decrypts the 3 DES-keys using private key
+5 - encrypt and decrypt messages between the client and server using the 3 DES-keys
+*/
